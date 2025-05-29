c3f09078fc969f1823ffed65553231d72116ac7e
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-29 17:47:42 +0200

    skapar en service för booking med interfaces

---
+++ src/app/services/booking.service.ts
@@ -0,0 +1,30 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+
+export interface BookingRequest {
+  name: string;
+  email: string;
+  guests: number;
+  date: string;
+  time: string;
+}
+
+export interface BookingResponse {
+  message: string;
+  bookingId: number;
+  assignedTable: number;
+}
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BookingService {
+  private apiUrl = 'http://localhost:5000/api/bookings';
+
+  constructor(private http: HttpClient) {}
+
+  createBooking(data: BookingRequest): Observable<BookingResponse> {
+    return this.http.post<BookingResponse>(this.apiUrl, data);
+  }
+}
