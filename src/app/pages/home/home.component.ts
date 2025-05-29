f5875bc96e49b8f34bb96e10fd76e010f016d6f9
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-29 19:47:33 +0200

    kommentarer innan jag glömmer

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -10,21 +10,31 @@ import { MessageService, ContactMessage } from '../../services/message.service';
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
+  // Kontaktformulärets data binds via ngmodel i HTML
   contact: ContactMessage = { name: '', email: '', message: '' };
+
+  // En sträng visas om textformuläret skickats korrekt, annars null, initieras som null
   successMessage: string | null = null;
+  // Samlar felmeddelanden i en array
   errorMessages: string[] = [];
 
+  // Använder en service för att hantera anrop till API
   constructor(private messageService: MessageService) {}
 
   onSubmit() {
+
+    // Nollställ felmeddelanden eller successmeddelanden
     this.errorMessages = [];
     this.successMessage = null;
 
+    // Använd servicen för att skicka data till backend, observable för att subscribe
     this.messageService.sendMessage(this.contact).subscribe({
+      // Om lyckat
       next: (res) => {
         this.successMessage = res.message;
         this.contact = { name: '', email: '', message: '' };
       },
+      // Om det misslyckades
       error: (err) => {
         if (err.error?.errors) {
           this.errorMessages = err.error.errors;
@@ -34,12 +44,15 @@ export class HomeComponent {
       }
     });
   }
-  showContact = false;
 
+  // Formuläret visas inte som utgångspunkt
+  showContact = false;
+// Växlar synlighet för formuläret
 toggleContact() {
   this.showContact = !this.showContact;
 }
 
+// Getter för errorMessages i HTML
 get errors(): string[] {
   return this.errorMessages;
 }
