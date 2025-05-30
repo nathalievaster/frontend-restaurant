ea20360c5f1289cde1bea0d4306a4b8781df5eca
Nathalie Fredriksson <nfredriksson1@gmail.com>
2025-05-30 11:10:42 +0200

    fixade till routermodule samt en till sektion

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -2,10 +2,11 @@ import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { MessageService, ContactMessage } from '../../services/message.service';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [ CommonModule, FormsModule],
+  imports: [ CommonModule, FormsModule, RouterModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
